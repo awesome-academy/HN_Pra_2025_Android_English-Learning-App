6df88d44457597bcd87f6c83dc01ed86388f9a7e
kiennv <kiennv.contact@gmail.com>
2025-08-14 00:37:51 +0700

    feat: add collection words

---
+++ app/src/main/java/com/sun/englishlearning/data/model/SavedWord.kt
@@ -0,0 +1,10 @@
+package com.sun.englishlearning.data.model
+
+data class SavedWord(
+    val id: String = "",
+    val word: String = "",
+    val definition: String = "",
+    val soundUrl: String = "",
+    val example: String = "",
+    val isFavorite: Boolean = true
+)

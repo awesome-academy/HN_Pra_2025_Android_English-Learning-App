6c7547f1c462f44d2e8845626e92369bb4e40988
NgDucAn <annd2003.vn@gmail.com>
2025-08-13 09:54:29 +0700

    remove blank line, fix leak memory

--- app/src/main/java/com/sun/englishlearning/screen/onboarding/fragment/WelComeFragment.kt
+++ app/src/main/java/com/sun/englishlearning/screen/onboarding/fragment/WelComeFragment.kt
@@ -1,15 +1,15 @@
 package com.sun.englishlearning.screen.onboarding.fragment
 
-
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
-import com.sun.englishlearning.databinding.FragmentWelComeBinding
-import com.sun.englishlearning.utils.base.BaseFragment
-import android.os.Handler
-import android.os.Looper
 import com.sun.englishlearning.R
+import com.sun.englishlearning.databinding.FragmentWelComeBinding
 import com.sun.englishlearning.utils.SharePreference
+import com.sun.englishlearning.utils.base.BaseFragment
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class WelComeFragment : BaseFragment<FragmentWelComeBinding>() {
 
@@ -22,12 +22,13 @@ class WelComeFragment : BaseFragment<FragmentWelComeBinding>() {
     ): FragmentWelComeBinding = FragmentWelComeBinding.inflate(inflater, container, false)
 
     override fun initData() {
-        Handler(Looper.getMainLooper()).postDelayed({
-            if (!isAdded) return@postDelayed
+        viewLifecycleOwner.lifecycleScope.launch {
+            delay(2000)
+            if (!isAdded) return@launch
             val seen = SharePreference.isOnboardingSeen(requireContext())
             val actionId = if (seen) R.id.action_welcome_to_home else R.id.action_welcome_to_onboarding
             findNavController().navigate(actionId)
-        }, 2000)
+        }
     }
 
     override fun initView() {
@@ -35,4 +36,3 @@ class WelComeFragment : BaseFragment<FragmentWelComeBinding>() {
     }
 
 }
-

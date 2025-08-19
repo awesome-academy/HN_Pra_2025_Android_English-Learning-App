00e283d87e33f8eede68152b61e6a170958f2a7b
LongVB <longvb@zyyx.jp>
2025-08-19 14:06:47 +0700

    fix: enhance onboarding experience and UI improvements

--- app/src/main/java/com/sun/englishlearning/screen/onboarding/OnboardingActivity.kt
+++ app/src/main/java/com/sun/englishlearning/screen/onboarding/OnboardingActivity.kt
@@ -1,15 +1,11 @@
 package com.sun.englishlearning.screen.onboarding
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.LayoutInflater
 import androidx.activity.enableEdgeToEdge
-import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.sun.englishlearning.R
-
-import android.content.Intent
-import android.view.LayoutInflater
-import com.google.android.material.tabs.TabLayoutMediator
 import com.sun.englishlearning.databinding.ActivityOnboardingBinding
 import com.sun.englishlearning.screen.login.LoginActivity
 import com.sun.englishlearning.screen.register.RegisterActivity
@@ -17,6 +13,17 @@ import com.sun.englishlearning.utils.AppPreferences
 import com.sun.englishlearning.utils.base.BaseActivity
 
 class OnboardingActivity : BaseActivity<ActivityOnboardingBinding>() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        enableEdgeToEdge()
+        super.onCreate(savedInstanceState)
+
+        ViewCompat.setOnApplyWindowInsetsListener(binding.root) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
 
     override fun inflateBinding(inflater: LayoutInflater): ActivityOnboardingBinding {
         return ActivityOnboardingBinding.inflate(inflater)
@@ -28,15 +35,19 @@ class OnboardingActivity : BaseActivity<ActivityOnboardingBinding>() {
 
         binding.btnGetStarted.setOnClickListener {
             // Mark as onboarding viewed
-            AppPreferences.setFirstLaunch(this, true)
+            AppPreferences.setFirstLaunch(this, false)
 
-            // Navigate to the login screen
+            // Navigate to the register screen
             val intent = Intent(this, RegisterActivity::class.java)
             startActivity(intent)
             finish()
         }
         
         binding.tvLogin.setOnClickListener {
+            // Mark as onboarding viewed
+            AppPreferences.setFirstLaunch(this, false)
+
+            // Navigate to the login screen
             val intent = Intent(this, LoginActivity::class.java)
             startActivity(intent)
             finish()

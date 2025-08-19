95e243747383f40eee8437b1a5593bf8b3356bd3
Nauh24 <nvnauh@gmail.com>
2025-08-19 14:35:07 +0700

    chore: remove unused imports

--- app/src/main/java/com/sun/englishlearning/screen/courses/CoursesFragment.kt
+++ app/src/main/java/com/sun/englishlearning/screen/courses/CoursesFragment.kt
@@ -1,11 +1,6 @@
 package com.sun.englishlearning.screen.courses
 
-import android.app.Activity
-import android.content.Intent
-import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.core.content.ContextCompat
--- app/src/main/java/com/sun/englishlearning/screen/lessondetail/LessonDetailFragment.kt
+++ app/src/main/java/com/sun/englishlearning/screen/lessondetail/LessonDetailFragment.kt
@@ -1,9 +1,7 @@
 package com.sun.englishlearning.screen.lessondetail
 
-import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.navigation.fragment.findNavController

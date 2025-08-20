aa60df8c7a3f7572e731b381514244c5f1fbab17
Nauh24 <nvnauh@gmail.com>
2025-08-20 11:01:24 +0700

    fix/api: fix hardcode

--- app/src/main/java/com/sun/englishlearning/data/repository/DictionaryRepository.kt
+++
@@ -1,8 +0,0 @@
-package com.sun.englishlearning.data.repository
-
-import com.sun.englishlearning.data.model.WordSearchResult
-
-interface DictionaryRepository {
-    suspend fun searchWord(query: String): Result<WordSearchResult>
-}
-
